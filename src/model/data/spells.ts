ca0595cc4df9ce9494d81a8ec050ec373d5f6e19
VaultDweller13 <45403328+VaultDweller13@users.noreply.github.com>
2023-02-17 13:52:09 +0400

    feat: add cast method to spells

--- src/model/data/spells.ts
+++ src/model/data/spells.ts
@@ -8,6 +8,7 @@ export const mage: Spell[] = [
     target: 'self',
     description: 'Shows map',
     type: 'maze',
+    cast: () => {},
   },
   {
     name: 'HALITO',
@@ -15,6 +16,7 @@ export const mage: Spell[] = [
     target: 'monster',
     description: `Causes a fireball to hit a monster for 1-8 points of fire damage`,
     type: 'battle',
+    cast: () => {},
   },
   {
     name: 'KATINO',
@@ -22,6 +24,7 @@ export const mage: Spell[] = [
     target: 'group',
     description: `Causes most of the monsters in a group to fall asleep`,
     type: 'battle',
+    cast: () => {},
   },
   {
     name: 'MOGREF',
@@ -29,6 +32,7 @@ export const mage: Spell[] = [
     target: 'self',
     description: `Reduces the caster's armor class by 2 for the encounter`,
     type: 'battle',
+    cast: () => {},
   },
   // Level 2
   {
@@ -37,6 +41,7 @@ export const mage: Spell[] = [
     target: 'group',
     description: `Causes one group of monsters to be enveloped in darkness, lowering their defense`,
     type: 'battle',
+    cast: () => {},
   },
   {
     name: 'SOPIC',
@@ -44,6 +49,7 @@ export const mage: Spell[] = [
     target: 'self',
     description: `Causes the caster to become transparent, thus reducing their armor class by 4`,
     type: 'battle',
+    cast: () => {},
   },
   // Level 3
   {
@@ -52,6 +58,7 @@ export const mage: Spell[] = [
     target: 'group',
     description: `Causes an explosion in a monster group, doing 4-24 points of fire damage`,
     type: 'battle',
+    cast: () => {},
   },
   {
     name: 'MOLITO',
@@ -59,6 +66,7 @@ export const mage: Spell[] = [
     target: 'group',
     description: `Causes sparks to damage half of the monsters in a group for 3-18 points damage`,
     type: 'battle',
+    cast: () => {},
   },
   // Level 4
   {
@@ -67,6 +75,7 @@ export const mage: Spell[] = [
     target: 'group',
     description: `Does 6-36 points of cold damage`,
     type: 'battle',
+    cast: () => {},
   },
   {
     name: 'LAHALITO',
@@ -74,6 +83,7 @@ export const mage: Spell[] = [
     target: 'group',
     description: `Does 6-36 points of fire damage`,
     type: 'battle',
+    cast: () => {},
   },
   {
     name: 'MORLIS',
@@ -81,6 +91,7 @@ export const mage: Spell[] = [
     target: 'group',
     description: `Causes a group of monsters to fear the party`,
     type: 'battle',
+    cast: () => {},
   },
   // Level 5
   {
@@ -89,6 +100,7 @@ export const mage: Spell[] = [
     target: 'group',
     description: `Causes 8-64 points of cold damage`,
     type: 'battle',
+    cast: () => {},
   },
   {
     name: 'MAKANITO',
@@ -96,6 +108,7 @@ export const mage: Spell[] = [
     target: 'all',
     description: `Kills low-level monsters`,
     type: 'battle',
+    cast: () => {},
   },
   {
     name: 'MAMORLIS',
@@ -103,6 +116,7 @@ export const mage: Spell[] = [
     target: 'all',
     description: `Causes all monsters to fear the party`,
     type: 'battle',
+    cast: () => {},
   },
   // Level 6
   {
@@ -111,6 +125,7 @@ export const mage: Spell[] = [
     target: 'party',
     description: `Not implemented`,
     type: 'battle',
+    cast: () => {},
   },
   {
     name: 'LAKANITO',
@@ -118,6 +133,7 @@ export const mage: Spell[] = [
     target: 'group',
     description: `Attempts to kill a group of monsters`,
     type: 'battle',
+    cast: () => {},
   },
   {
     name: 'MASOPIC	',
@@ -125,6 +141,7 @@ export const mage: Spell[] = [
     target: 'party',
     description: `Reduces the armor class of the entire party by 4`,
     type: 'battle',
+    cast: () => {},
   },
   {
     name: 'ZILWAN	',
@@ -132,6 +149,7 @@ export const mage: Spell[] = [
     target: 'monster',
     description: `Will destroy one undead monster`,
     type: 'battle',
+    cast: () => {},
   },
   // Level 7
   {
@@ -140,6 +158,7 @@ export const mage: Spell[] = [
     target: 'party',
     description: `Not implemented`,
     type: 'battle',
+    cast: () => {},
   },
   {
     name: 'MALOR',
@@ -147,6 +166,7 @@ export const mage: Spell[] = [
     target: 'party',
     description: `Teleports party to the specified location`,
     type: 'any',
+    cast: () => {},
   },
   {
     name: 'TILTOWAIT',
@@ -154,10 +174,11 @@ export const mage: Spell[] = [
     target: 'all',
     description: `Does 10-100 hit points of damage to all monster`,
     type: 'battle',
+    cast: () => {},
   },
 ];
 
-export const priest = [
+export const priest: Spell[] = [
   // Level 1
   {
     name: 'badios',
@@ -165,6 +186,7 @@ export const priest = [
     target: 'monster',
     description: 'Causes 1-8 points of damage to a monster',
     type: 'battle',
+    cast: () => {},
   },
   {
     name: 'dios',
@@ -172,6 +194,7 @@ export const priest = [
     target: 'character',
     description: 'Restores 1-8 points of health to a party member',
     type: 'any',
+    cast: () => {},
   },
   {
     name: 'KALKI',
@@ -179,6 +202,7 @@ export const priest = [
     target: 'party',
     description: `Reduces the armor class of all party members by 1 during combat`,
     type: 'battle',
+    cast: () => {},
   },
   {
     name: 'MILWA',
@@ -186,6 +210,7 @@ export const priest = [
     target: 'party',
     description: `Summons a softly glowing light that increases vision and reveals secret doors`,
     type: 'maze',
+    cast: () => {},
   },
   {
     name: 'PORFIC',
@@ -193,6 +218,7 @@ export const priest = [
     target: 'self',
     description: 'Lowers the armor class of the caster by 4 during combat',
     type: 'battle',
+    cast: () => {},
   },
   // Level 2
   {
@@ -201,6 +227,7 @@ export const priest = [
     target: 'self',
     description: 'Reveals the trap on a treasure chest',
     type: 'maze',
+    cast: () => {},
   },
   {
     name: 'MANIFO',
@@ -208,6 +235,7 @@ export const priest = [
     target: 'group',
     description: 'Causes some of the monsters to become temporarily paralyzed',
     type: 'battle',
+    cast: () => {},
   },
   {
     name: 'MATU',
@@ -215,6 +243,7 @@ export const priest = [
     target: 'party',
     description: 'Lowers armor class of all party members by 2 during combat',
     type: 'battle',
+    cast: () => {},
   },
   {
     name: 'MONTINO',
@@ -222,6 +251,7 @@ export const priest = [
     target: 'group',
     description: `Silences the air around a group of monsters, making it impossible for them to cast spells`,
     type: 'battle',
+    cast: () => {},
   },
   // Level 3
   {
@@ -230,6 +260,7 @@ export const priest = [
     target: 'party',
     description: `Lowers the party's armor class by 4 in combat`,
     type: 'battle',
+    cast: () => {},
   },
   {
     name: 'DIALKO',
@@ -237,6 +268,7 @@ export const priest = [
     target: 'character',
     description: 'Cures paralysis, and cures the effects of MANIFO and KATINO',
     type: 'any',
+    cast: () => {},
   },
   {
     name: 'LATUMAPIC',
@@ -244,6 +276,7 @@ export const priest = [
     target: 'group',
     description: 'Identifies unknown monsters',
     type: 'maze',
+    cast: () => {},
   },
   {
     name: 'LOMILWA',
@@ -251,6 +284,7 @@ export const priest = [
     target: 'party',
     description: `Extends field of vision and reveals secret doors. Lasts until leaving the maze or entering a dark area.`,
     type: 'maze',
+    cast: () => {},
   },
   // Level 4
   {
@@ -259,6 +293,7 @@ export const priest = [
     target: 'monster',
     description: `Causes 2-16 points of damage`,
     type: 'battle',
+    cast: () => {},
   },
   {
     name: 'DIALKO',
@@ -266,6 +301,7 @@ export const priest = [
     target: 'character',
     description: 'Restores 2-16 points of health',
     type: 'any',
+    cast: () => {},
   },
   {
     name: 'LATUMOFIS	',
@@ -273,6 +309,7 @@ export const priest = [
     target: 'character',
     description: 'Removes the effects of poison',
     type: 'any',
+    cast: () => {},
   },
   {
     name: 'MAPORFIC	',
@@ -280,6 +317,7 @@ export const priest = [
     target: 'party',
     description: `Lowers the party's armor class by 2, and lasts for the entire expedition`,
     type: 'maze',
+    cast: () => {},
   },
   // Level 5
   {
@@ -288,6 +326,7 @@ export const priest = [
     target: 'monster',
     description: `Attempt to kill one monster`,
     type: 'battle',
+    cast: () => {},
   },
   {
     name: 'BADIALMA',
@@ -295,6 +334,7 @@ export const priest = [
     target: 'monster',
     description: 'Causes 3-24 points of damage',
     type: 'battle',
+    cast: () => {},
   },
   {
     name: 'DI	',
@@ -302,6 +342,7 @@ export const priest = [
     target: 'character',
     description: 'Causes a dead person to be resurrected. ',
     type: 'any',
+    cast: () => {},
   },
   {
     name: 'DIALMA	',
@@ -309,6 +350,7 @@ export const priest = [
     target: 'character',
     description: `Restores 3-24 points of health`,
     type: 'any',
+    cast: () => {},
   },
   {
     name: 'KANDI	',
@@ -316,6 +358,7 @@ export const priest = [
     target: 'self',
     description: `Gives the direction of the person the party is attempting to locate, relative to the position of the caster`,
     type: 'maze',
+    cast: () => {},
   },
   {
     name: 'LITOKAN',
@@ -323,6 +366,7 @@ export const priest = [
     target: 'group',
     description: `Causes a pillar of flame to strike a group of monsters, doing 3-24 points of damage`,
     type: 'battle',
+    cast: () => {},
   },
   // Level 6
   {
@@ -331,6 +375,7 @@ export const priest = [
     target: 'party',
     description: `Causes all party members to be transported back to the castle, minus all of their equipment and most of their gold`,
     type: 'maze',
+    cast: () => {},
   },
   {
     name: 'LORTO',
@@ -338,6 +383,7 @@ export const priest = [
     target: 'group',
     description: `Causes sharp blades to slice through a group, causing 6-36 points of damage`,
     type: 'battle',
+    cast: () => {},
   },
   {
     name: 'MABADI',
@@ -345,6 +391,7 @@ export const priest = [
     target: 'monster',
     description: `Causes all but 1-8 hit points to be removed from a target`,
     type: 'battle',
+    cast: () => {},
   },
   {
     name: 'MADI',
@@ -352,6 +399,7 @@ export const priest = [
     target: 'character',
     description: `Causes all hit points to be restored and cures any condition except death`,
     type: 'any',
+    cast: () => {},
   },
   // Level 7
   {
@@ -360,6 +408,7 @@ export const priest = [
     target: 'character',
     description: `Restores the dead to life, and restores all hit points, even if the character is ashes. However, if the spell fails, the character is permanently lost.`,
     type: 'any',
+    cast: () => {},
   },
   {
     name: 'MALIKTO',
@@ -367,5 +416,6 @@ export const priest = [
     target: 'all',
     description: `Causes 12-72 hit points of damage to all monsters`,
     type: 'battle',
+    cast: () => {},
   },
 ];
