16d1bdcb4e0ea9348559644ddc44bb5ecd70ccfb
VaultDweller13 <grognak13@gmail.com>
2023-02-24 05:15:21 +0400

    feat: change layout

--- src/View/Render/renderBasic.ts
+++ src/View/Render/renderBasic.ts
@@ -3,6 +3,7 @@
 // }
 import { getMain, createElement } from './common';
 import enterCastle from './castle/castle';
+import logo from '../Assets/rsschool-logo.svg';
 
 function startGame() {
   const main = getMain();
@@ -14,23 +15,22 @@ function startGame() {
 function renderBasic() {
   const { body } = document;
   body.innerHTML = `
-  <main class="main">
-    <div class="block">
-      <button class="button" id="start-game">Start Game</button>
-    </div>
-  </main>
-  <footer class="footer">
+  <section class="footer-left">
     <a id="rs-link" href="https://rs.school/js/">
-      <img id="rs-image" src="https://rs.school/images/rs_school_js.svg">
+      <img id="rs-image" src=${logo as string}>
     </a>
+  </section>
+  <main class="main">
+      <button class="button button-start" id="start-game">Start Game</button>
+  </main>
+  <section class="footer-right">
     <div id="year">2023</div>
     <ul id="github-list">
-      <li><a class="link" href="https://github.com/VaultDweller13">Artem Bagaev</a></li>
+      <li><a class="link" href="https://github.com/VaultDweller13">Artyom Bagaev</a></li>
       <li><a class="link" href="https://github.com/oryngalikarimzhan">Oryngali Karimzhan</a></li>
       <li><a class="link" href="https://github.com/JaLongin">Vasili Drachou</a></li>
     </ul>
-  </footer>
-  `;
+  </section>`;
   const startButton = document.getElementById('start-game');
   startButton?.addEventListener('click', startGame);
 }
